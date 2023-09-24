7424bbbc65b54b4d72c8d2b234128e5a0e50e8cb
b00db <buyeong21@gmail.com>
2023-09-24 20:20:49 +0900

    [ADD] Birthday Screen

--- lib/features/authentication/birthday_screen.dart
+++ lib/features/authentication/birthday_screen.dart
@@ -1,10 +1,125 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:tiktok_clone/constants/gaps.dart';
+import 'package:tiktok_clone/constants/sizes.dart';
+import 'package:tiktok_clone/features/authentication/widgets/form_button.dart';
+import 'package:tiktok_clone/features/onboarding/interests_screen.dart';
 
-class BirthdayScreen extends StatelessWidget {
+class BirthdayScreen extends StatefulWidget {
   const BirthdayScreen({super.key});
 
   @override
+  State<BirthdayScreen> createState() => _BirthdayScreenState();
+}
+
+class _BirthdayScreenState extends State<BirthdayScreen> {
+  final TextEditingController _birthdayController = TextEditingController();
+  DateTime initialDate =
+      DateTime.now().subtract(const Duration(days: 365 * 12));
+
+  @override
+  void initState() {
+    super.initState();
+    _setTextFieldDate(initialDate);
+  }
+
+  @override
+  void dispose() {
+    _birthdayController.dispose();
+    super.dispose();
+  }
+
+  void _onScaffoldTap() {
+    FocusScope.of(context).unfocus();
+  }
+
+  void _onNextTap() {
+    // if (_username.isEmpty) return;
+    Navigator.of(context).push(
+      MaterialPageRoute(
+        builder: (context) => const InterestsScreen(),
+      ),
+    );
+  }
+
+  void _setTextFieldDate(DateTime date) {
+    final textDate = date.toString().split(" ").first;
+    _birthdayController.value = TextEditingValue(text: textDate);
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold(body: Placeholder());
+    return GestureDetector(
+      onTap: _onScaffoldTap,
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text(
+            'Sign up',
+          ),
+        ),
+        body: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: Sizes.size36),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Gaps.v20,
+              const Text(
+                "When's your birthday?",
+                style: TextStyle(
+                  fontSize: Sizes.size20 + Sizes.size2,
+                  fontWeight: FontWeight.w700,
+                ),
+              ),
+              Gaps.v8,
+              const Text(
+                "Your birthday won't be shown publicly.",
+                style: TextStyle(
+                  fontSize: Sizes.size16,
+                  color: Colors.black54,
+                ),
+              ),
+              Gaps.v16,
+              TextField(
+                enabled: false,
+                controller: _birthdayController,
+                onEditingComplete: _onNextTap,
+                decoration: InputDecoration(
+                  enabledBorder: UnderlineInputBorder(
+                    borderSide: BorderSide(
+                      color: Colors.grey.shade400,
+                    ),
+                  ),
+                  focusedBorder: UnderlineInputBorder(
+                    borderSide: BorderSide(
+                      color: Colors.grey.shade400,
+                    ),
+                  ),
+                ),
+                cursorColor: Theme.of(context).primaryColor,
+                style: const TextStyle(
+                  color: Colors.black,
+                ),
+              ),
+              Gaps.v28,
+              GestureDetector(
+                onTap: _onNextTap,
+                child: const FormButton(disabled: false),
+              )
+            ],
+          ),
+        ),
+        bottomNavigationBar: BottomAppBar(
+          child: SizedBox(
+            height: 300,
+            child: CupertinoDatePicker(
+              mode: CupertinoDatePickerMode.date,
+              maximumDate: initialDate,
+              initialDateTime: initialDate,
+              onDateTimeChanged: (date) => _setTextFieldDate(date),
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
